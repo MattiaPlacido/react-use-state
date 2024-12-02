955043e4fb9dcb509bf3ef8df56961e0ac369734
MattiaPlacido <placido.mattia@gmail.com>
2024-12-02 19:17:06 +0100

    Commenti

--- vite-project/src/components/Main.jsx
+++ vite-project/src/components/Main.jsx
@@ -7,16 +7,21 @@ export default function Main() {
   return (
     <div className="main">
       <div className="button-container">
+        {/* per ogni linguaggio stampo il bottone corrispondente */}
         {languages.map((language) => (
           <button
             onClick={() => {
+              //Assegno il linguaggio attuale ad una variabile per passarlo
               const clickedLanguage = language;
+              //controllo che non sia gia attivo
               if (clickedLanguage === activeLanguage) {
                 alert("Questo linguaggio sta già venendo mostrato");
               } else {
+                //setto il linguaggio cliccato come attivo
                 setLanguage(clickedLanguage);
               }
             }}
+            //se l'id del linguaggio corrisponde con quello attivo viene assegnata la classe active
             className={`button ${
               language.id === activeLanguage.id ? "active" : ""
             }`}
